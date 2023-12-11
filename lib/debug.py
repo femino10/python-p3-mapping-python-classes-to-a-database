6724cc1376f5e468e2ab7785670cfa1581427868
felix <ndirangufelix10@gmail.com>
2023-12-11 07:45:46 +0300

    completed assignment

--- lib/debug.py
+++ lib/debug.py
@@ -5,4 +5,8 @@ from song import Song
 
 
 if __name__ == '__main__':
+    Song.create_table()
+    song = Song('The Sound of Silence', 'The Sound of Silence')
+    song.save()
+    CONN.commit()
     import ipdb; ipdb.set_trace()
